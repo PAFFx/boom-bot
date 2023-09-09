a688faf1d4d0713d2159b44923cbd2f5186e3ba6
paff <jj2jom@gmail.com>
2023-09-09 13:44:32 +0700

    add params to lidar launch file

--- launch/hlslidar.launch.py
+++ launch/hlslidar.launch.py
@@ -1,20 +1,38 @@
-import os
-
-from launch import LaunchDescription
+from launch import LaunchContext, LaunchDescription
 from launch.launch_introspector import ExecuteProcess
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 
-def generate_launch_description():
-
+def launch_setup(context: LaunchContext):
+    port = LaunchConfiguration('port').perform(context)
+    frame_id = LaunchConfiguration('frame_id').perform(context)
     
     package = 'hls_lfcd_lds_driver'
     launch_file = 'hlds_laser.launch.py'
 
-    return LaunchDescription([
-        ExecuteProcess(
+    lidar = ExecuteProcess(
             cmd=[[
-                'ros2 launch ',package,' ',launch_file,
-                ' frame_id:=laser_frame', ' port:=/dev/ttyUSB0'
+                f"ros2 launch {package} {launch_file} frame_id:={frame_id} port:={port}"
                 ]],
             shell=True
             )
+    return [lidar]
+
+def generate_launch_description():
+
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'port',
+            default_value='/dev/ttyUSB0',
+            description='Lidar\'s port'
+            ),
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value='laser_frame',
+            description='laser frame name'
+            ),
+        OpaqueFunction(function=launch_setup)
+
         ])
