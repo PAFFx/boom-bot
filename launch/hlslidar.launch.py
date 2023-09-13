32a3dbda2581d19cce18d793f44597a5491bc82a
paff <jj2jom@gmail.com>
2023-09-13 16:18:54 +0700

    remove laser filter

--- launch/hlslidar.launch.py
+++ launch/hlslidar.launch.py
@@ -1,12 +1,7 @@
-import os
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchContext, LaunchDescription
 from launch.launch_introspector import ExecuteProcess
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
-
-from launch_ros.actions import Node
 
 def launch_setup(context: LaunchContext):
     port = LaunchConfiguration('port').perform(context)
@@ -24,15 +19,6 @@ def launch_setup(context: LaunchContext):
     return [lidar]
 
 def generate_launch_description():
-    
-    package_name="boom_bot"
-    laser_filter_params = os.path.join(get_package_share_directory(package_name), "config", "laser_filter.yaml")
-
-    laser_filter=  Node(
-            package="laser_filters",
-            executable="scan_to_scan_filter_chain",
-            parameters=[{"load":laser_filter_params}],
-            )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -46,6 +32,5 @@ def generate_launch_description():
             description='laser frame name'
             ),
         OpaqueFunction(function=launch_setup),
-        laser_filter
 
         ])
