22e97712efba0f8174c65916dd623fc6953f6821
paff <jj2jom@gmail.com>
2023-09-13 13:28:15 +0700

    add scan filtered

--- launch/hlslidar.launch.py
+++ launch/hlslidar.launch.py
@@ -1,7 +1,12 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchContext, LaunchDescription
 from launch.launch_introspector import ExecuteProcess
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
 
 def launch_setup(context: LaunchContext):
     port = LaunchConfiguration('port').perform(context)
@@ -19,8 +24,15 @@ def launch_setup(context: LaunchContext):
     return [lidar]
 
 def generate_launch_description():
+    
+    package_name="boom_bot"
+    laser_filter_params = os.path.join(get_package_share_directory(package_name), "config", "laser_filter.yaml")
 
-
+    laser_filter=  Node(
+            package="laser_filters",
+            executable="scan_to_scan_filter_chain",
+            parameters=[{"load":laser_filter_params}],
+            )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -33,6 +45,7 @@ def generate_launch_description():
             default_value='laser_frame',
             description='laser frame name'
             ),
-        OpaqueFunction(function=launch_setup)
+        OpaqueFunction(function=launch_setup),
+        laser_filter
 
         ])
