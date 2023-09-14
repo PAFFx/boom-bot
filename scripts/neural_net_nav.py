891ee175a517db3319a7ec4fb82e1e92c19d570e
paff <jj2jom@gmail.com>
2023-09-14 19:18:23 +0700

    use custom state machine

--- scripts/neural_net_nav.py
+++ scripts/neural_net_nav.py
@@ -8,46 +8,66 @@ from rclpy.duration import Duration
 
 from custom_msgs.msg import VisualNav
 from geometry_msgs.msg import PoseStamped 
-from builtin_interfaces.msg import  Time
 
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult, Spin
 
 class NeuralNetNav(Node):
-
+    
     def __init__(self):
         super().__init__('neural_net_nav')
+
+        self.navigator = BasicNavigator()
+        self.state = "wait" # wait, follow, stop
+
+        # TODO: set_inital pose
+        # self.navigator.setInitialPose(init_pose)
+        
+        # Wait for navigation to fully activate. Use this line if autostart is set to true.
+        self.navigator.waitUntilNav2Active()
 
         # Horizontal FOV : 60 deg, Horizontal Res : 1280
         self.fov = 60
         self.h_res = 1280
         self.frame_id = "base_link"
+        self.last_goal_msg = PoseStamped()
 
         self.subscription = self.create_subscription(
             VisualNav,
             '/visual_nav',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
 
-        self.publisher = self.create_publisher(
-            PoseStamped,
-            '/goal_pose',
-            10)
-        self.publisher # prevent unused variable warning
+    def listener_callback(self, msg : VisualNav):
+        op = msg.operation
+        if op == "follow":
+            self.state="follow"
+            self.follow_state(msg)
+        elif op == "wait":
+            self.state = "wait"
+            self.wait_state()
+
+        elif op == "follow" and self.navigator.getResult() == TaskResult.SUCCEEDED:
+            self.state = "spin"
+            self.navigator.spin()
+
+        else:
+            print("Invalid operation command")
 
 
-    def listener_callback(self, msg):
+
+
+
+
+    def follow_state(self, msg):
         x_pos,y_pos = self.calc_robot_position_goal(msg.horizontal_center_offset, msg.distance)
-        self.get_logger().info(f'X,Y Goal : {x_pos}, {y_pos}')
-            
-        time_obj = Time()
-        time_obj.sec = self.get_clock().now().seconds_nanoseconds()[0]
+        # Publish goal
+        self.last_goal_msg.header.stamp = self.navigator.get_clock().now()
+        self.last_goal_msg.header.frame_id = self.frame_id
+        self.last_goal_msg.pose.position.x = x_pos
+        self.last_goal_msg.pose.position.y = y_pos
+        #TODO: Add orientation
 
-        # publish goal
-        goal_msg = PoseStamped()
-        goal_msg.header.stamp = time_obj
-        goal_msg.header.frame_id = self.frame_id
-        goal_msg.pose.position.x = x_pos
-        goal_msg.pose.position.y = y_pos
+        self.navigator.goToPose([self.last_goal_msg])
 
     def calc_robot_position_goal(self, x_offset,distance):
         deg_offset = ( x_offset * self.fov)/ (self.h_res)
@@ -57,19 +77,25 @@ class NeuralNetNav(Node):
         y_pos = distance * math.sin(rad_offset)
 
         return x_pos, y_pos
-        
 
+    def wait_state(self):
+        self.last_goal_msg.header.stamp = self.navigator.get_clock().now()
+        self.last_goal_msg.header.frame_id = self.frame_id
+        self.last_goal_msg.pose.position.x = 0
+        self.last_goal_msg.pose.position.y = 0
+
+        self.navigator.goToPose([self.last_goal_msg])
+    
+        
 
 def main(args=None):
     rclpy.init(args=args)
 
     neural_net_nav = NeuralNetNav()
-
     rclpy.spin(neural_net_nav)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+
+
     neural_net_nav.destroy_node()
     rclpy.shutdown()
 
