7c57a33a65d9e50b6aadb0e5b47f08692bedaa22
paff <jj2jom@gmail.com>
2023-09-16 15:07:19 +0700

    use simple navigator for neural net nav

--- scripts/neural_net_nav.py
+++ scripts/neural_net_nav.py
@@ -4,11 +4,13 @@ import time  # Time library
 import rclpy
 from rclpy.node import Node, math
 from rclpy.duration import Duration
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
 
 from custom_msgs.msg import VisualNav
-from geometry_msgs.msg import PoseStamped 
+from geometry_msgs.msg import PoseStamped, Twist 
 from sensor_msgs.msg import Image 
+from builtin_interfaces.msg import Time
 
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult, Spin
 
@@ -17,94 +19,123 @@ class NeuralNetNav(Node):
     def __init__(self):
         super().__init__('neural_net_nav')
 
-        self.navigator = BasicNavigator()
         self.state = "wait" # wait, follow, stop
-
-        # TODO: set_inital pose
-        # self.navigator.setInitialPose(init_pose)
-
-        self.depth_data = []
-        
-        # Wait for navigation to fully activate. Use this line if autostart is set to true.
-        self.navigator.waitUntilNav2Active()
+        self.command = VisualNav()
+        self.distance = 0.0
+        self.distance_treshold = 1.0
+        self.get_logger().info("wait")
 
         # Horizontal FOV : 60 deg, Horizontal Res : 1280
         self.fov = 60
         self.h_res = 1280
         self.v_res = 720
-        self.frame_id = "base_link"
-        self.last_goal_msg = PoseStamped()
 
+        # Create subscription
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+        print("create nav subscription")
         self.nav_subscription = self.create_subscription(
             VisualNav,
             '/visual_nav',
             self.nav_listener_callback,
             10)
-
+        print("create depth subscription")
         self.depth_subscription = self.create_subscription(
                 Image,
                 '/camera/depth/image_raw',
                 self.depth_listener_callback,
+                qos_profile
+                )
+
+        self.cmd_vel_publisher = self.create_publisher(
+                Twist,
+                '/cmd_vel',
                 10
                 )
 
+        self.timer = self.create_timer(
+                0.01,
+                self.timer_stateful_action_callback 
+                )
+
+    def timer_stateful_action_callback(self):
+        if self.state == "follow":
+            print("follow")
+            self.follow_state()
+        elif self.state == "wait":
+            print("wait")
+            self.wait_state()
+        
+        elif self.state == "notfound":
+            print("notfound")
+
+
     def depth_listener_callback(self, msg : Image):
-        print(msg.data)
         self.depth_data = msg.data
+        print(float(msg.data))
+        # self.get_logger().info(len(self.depth_data))
+
 
     def nav_listener_callback(self, msg : VisualNav):
-        op = msg.operation
-        if op == "follow":
+        self.command = msg
+        if self.command.operation == "follow":
             self.state="follow"
-            self.follow_state(msg)
-        elif op == "wait":
+        elif self.command.operation == "wait":
             self.state = "wait"
-            self.wait_state()
 
-        elif op == "notfound" and self.navigator.getResult() == TaskResult.SUCCEEDED:
-            self.state = "spin"
-            self.navigator.spin()
+        elif self.command.operation == "notfound":
+            self.state = "notfound"
 
         else:
             print("Invalid operation command")
 
 
-
-
-
-
-    def follow_state(self, msg : VisualNav):
+    def follow_state(self):
         # read depth
-        x_depth_pos = int((msg.x / self.h_res) * 640)
-        y_depth_pos = int((msg.y / self.v_res) * 480)
+        x_depth_pos = int((self.command.x / self.h_res) * 640)
+        y_depth_pos = int((self.command.y / self.v_res) * 480)
+        horizontal_error = self.command.x - self.h_res/2
         depth = 0
         try: 
-            depth = self.depth_data[640*x_depth_pos + y_depth_pos]
+            # depth = self.depth_data[(640*x_depth_pos + y_depth_pos) * 2]
+            depth = 100 # TODO: Implement depth
         except:
             print("Error reading depth data")
+        self.distance = depth
 
-        x_pos,y_pos = self.calc_robot_position_goal(msg.horizontal_center_offset, depth)
-        # Publish goal
-        self.last_goal_msg.header.stamp = self.navigator.get_clock().now()
-        self.last_goal_msg.header.frame_id = self.frame_id
-        self.last_goal_msg.pose.position.x = x_pos
-        self.last_goal_msg.pose.position.y = y_pos
-        #TODO: Add orientation
+        twist_msg = Twist()
+        # linear + angular
+        if self.distance > self.distance_treshold and horizontal_error != 0:
+            twist_msg.linear.x = 0.5
+            twist_msg.angular.z = - (horizontal_error / self.h_res) * 1.0
+        # linear
+        elif self.distance > self.distance_treshold:
+            twist_msg.linear.x = 1.0
+        # angular
+        elif horizontal_error != 0:
+            twist_msg.angular.z = -(horizontal_error / self.h_res ) * 1.0
 
-        self.navigator.goToPose([self.last_goal_msg])
+        
+        print("x:",twist_msg.linear.x)
+        print("z:",twist_msg.angular.z)
+        self.cmd_vel_publisher.publish(
+                twist_msg
+                )
 
-    def calc_robot_position_goal(self, x_offset,distance):
-        deg_offset = ( x_offset * self.fov)/ (self.h_res)
-        rad_offset = (deg_offset * math.pi) / 180
-
-        x_pos = distance * math.cos(rad_offset)
-        y_pos = distance * math.sin(rad_offset)
-
-        return x_pos, y_pos
+        
 
     def wait_state(self):
-        self.navigator.cancelTask()
-    
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.0
+        twist_msg.angular.z = 0.0
+        self.cmd_vel_publisher.publish(
+                twist_msg
+                )
+
+
         
 
 def main(args=None):
