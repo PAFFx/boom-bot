ca84fab22297ac74932f89afa5dea22628088261
paff <jj2jom@gmail.com>
2023-09-16 21:41:42 +0700

    remove depth camera usage

--- scripts/neural_net_nav.py
+++ scripts/neural_net_nav.py
@@ -15,21 +15,6 @@ from sensor_msgs.msg import Image
 from builtin_interfaces.msg import Time
 
 
-def construct_depth_image(raw, width, height):
-    arr = np.array(raw, dtype=np.uint8).reshape(-1, 2)
-    arr = arr[:, 0] + (arr[:, 1] * 2**8)
-    arr = arr.reshape(height, width)
-    return arr
-
-def average_depth(depth_image : np.ndarray, x,y) -> float:
-    print(depth_image.shape[0])
-    print(depth_image.shape[1])
-    depth_image = np.pad(depth_image,(10,), mode='mean')
-    res = np.average(depth_image[x-10:x+10, y-10:y+10])
-
-    return res.item()
-    
-
 class NeuralNetNav(Node):
     
     def __init__(self):
@@ -37,8 +22,13 @@ class NeuralNetNav(Node):
 
         self.state = "wait" # wait, follow, stop
         self.command = VisualNav()
-        self.distance_threshold = 500.0 # millimetres
-        self.horizontal_threshold = 300.0 # pixels
+        self.horizontal_threshold = 325.0 # pixels
+        self.timer_period = 0.01  #sec
+        self.wakeup_limit = 5
+
+
+        self.sleep_count = 0
+        self.wakeup_count = 0
 
         self.get_logger().info("wait")
 
@@ -47,25 +37,12 @@ class NeuralNetNav(Node):
         self.h_res = 1280
         self.v_res = 720
 
-        # Create subscription
-        qos_profile = QoSProfile(
-            reliability=QoSReliabilityPolicy.BEST_EFFORT,
-            history=QoSHistoryPolicy.KEEP_LAST,
-            depth=1
-        )
         self.get_logger().info("create nav subscription")
         self.nav_subscription = self.create_subscription(
             VisualNav,
             '/visual_nav',
             self.nav_listener_callback,
             10)
-        self.get_logger().info("create depth subscription")
-        self.depth_subscription = self.create_subscription(
-                Image,
-                '/camera/depth/image_raw',
-                self.depth_listener_callback,
-                qos_profile
-                )
 
         self.cmd_vel_publisher = self.create_publisher(
                 Twist,
@@ -74,11 +51,16 @@ class NeuralNetNav(Node):
                 )
 
         self.timer = self.create_timer(
-                0.01,
+                self.timer_period,
                 self.timer_stateful_action_callback 
                 )
 
     def timer_stateful_action_callback(self):
+        self.sleep_count += 1
+        if self.sleep_count >= 0.5/self.timer_period:
+            self.sleep_count = 0
+            self.state = "wait"
+
         if self.state == "follow":
             self.get_logger().info("follow")
             self.follow_state()
@@ -88,58 +70,53 @@ class NeuralNetNav(Node):
         
         elif self.state == "notfound":
             self.get_logger().info("notfound")
-            self.wait_state()
 
 
-    def depth_listener_callback(self, msg : Image):
-        self.depth_data = construct_depth_image(msg.data, msg.width, msg.height)
 
     def nav_listener_callback(self, msg : VisualNav):
+        self.sleep_count = 0
         self.command = msg
         if self.command.operation == "follow":
-            self.state="follow"
+            if self.state=="wait" and self.wakeup_count > self.wakeup_limit:
+                self.wakeup_count = 0
+                self.state="follow"
+
+            elif self.state == "wait":
+                self.wakeup_count += 1
+
+            else:
+                self.state ="follow"
         elif self.command.operation == "wait":
+            self.wakeup_count = 0
             self.state = "wait"
 
         elif self.command.operation == "notfound":
-            self.state = "notfound"
+            self.wakeup_count = 0
+            if self.state == "wait":
+                self.state = "wait"
+            else:
+                self.state = "notfound"
 
         else:
             self.get_logger().info("operation not support")
 
 
     def follow_state(self):
-        # read depth
-        x_depth_pos = int((self.command.x / self.h_res) * 640)
-        y_depth_pos = int((self.command.y / self.v_res) * 480)
-
         horizontal_error = self.command.x - self.h_res/2
-        depth = 0
-        try: 
-            depth = self.depth_data[x_depth_pos, y_depth_pos]
-            # depth = average_depth(self.depth_data, x_depth_pos, y_depth_pos)
-            # depth = 3000
-            self.get_logger().info(f"depth: {depth}")
-        except:
-            self.get_logger().info("Error reading depth data")
-
         twist_msg = Twist()
 
         # angular
         if horizontal_error > (self.h_res/2 - self.horizontal_threshold):
             twist_msg.linear.x = 0.0
-            twist_msg.angular.z = -1.0 # right
+            twist_msg.angular.z = -0.65 # right
         elif horizontal_error < -(self.h_res/2 -self.horizontal_threshold):
             twist_msg.linear.x = 0.0
-            twist_msg.angular.z = 1.0 # left
+            twist_msg.angular.z = 0.65 # left
 
         # linear + angular
-        elif depth > self.distance_threshold:
+        else:
             twist_msg.linear.x = 0.2
             twist_msg.angular.z = - (horizontal_error / self.h_res) * 1.0
-        else:
-            twist_msg.linear.x = 0.0
-            twist_msg.angular.z = 0.0
 
         
         self.get_logger().info(f"x: {self.command.x}")
